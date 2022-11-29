f567fda85b7c0e1e0fd0062e822152b4caeea310
Lukas <lun194223@gmail.com>
2022-11-29 13:42:11 +0100

    fix: fix

--- src/extensions/scratch3_ft/index.js
+++ src/extensions/scratch3_ft/index.js
@@ -42,19 +42,17 @@ function knopf() {  //Button der gedrückt wird ruft das auf
 	} else {
 		img.setAttribute("src", ftDisconnectedIcon);
 	} 
-	
         navigator.bluetooth.requestDevice({
             filters: [{ name: 'BT Smart Controller' }],
             optionalServices: ['8ae883b4-ad7d-11e6-80f5-76304dec7eb7', '8ae87702-ad7d-11e6-80f5-76304dec7eb7', '8ae8952a-ad7d-11e6-80f5-76304dec7eb7', '8ae88d6e-ad7d-11e6-80f5-76304dec7eb7', ]
         }).then(device => {
             console.log("Device found. Connecting ...");
-			
             return device.gatt.connect();       
         }).then(server => {
             console.log("Connected. Searching for output service ...");
             return server.getPrimaryServices() ;
         }).then(services => {
-            console.log("Service found. Requesting M1 characteristic ...");
+            console.log("Service found. Requesting characteristic ...");
 			console.log (services.map(s =>s.uuid).join('\n' + ' '.repeat(19)));
 			for(i=0; i<4; i=i+1){
 				console.log(i+services[i].uuid);
@@ -79,7 +77,6 @@ function knopf() {  //Button der gedrückt wird ruft das auf
 				i=10; }};
         }).then(characteristic => {
             console.log("Characteristic found.");
-            console.log("You can now use the M1 on/off checkbox.");
             characteristic.writeValue(new Uint8Array([1]));
             d=characteristic;
             return 5;
@@ -164,11 +161,9 @@ var f=0;
 
 class Scratch3FtBlocks {
 	
-	
 	setButton(state, msg=null) {
 		button = document.getElementById(FT_BUTTON_ID).addEventListener("click", knopf);
 	}
-	
 	
 	addButton(initial = true) {
 	//  check if the button already exists
@@ -226,71 +221,69 @@ class Scratch3FtBlocks {
 
 
 	   	blocks: [
-		{
-		    opcode: 'Motor',
-            blockType: BlockType.HAT,
-            text: "Motor",
-        },
-        b.getBlock(),
-		b.getBlock_setLamp(),
-		b.getBlock_doSetMotorSpeed(),
-		b.getBlock_doSetMotorSpeedDir(),
-		{
-		    opcode: 'hat',
-            blockType: BlockType.COMMAND,
-            text: "Motor",
-        },
-		{
-		    opcode: 'input',
-		    text: 'set [INPUT]',
-            blockType: BlockType.EVENT,
-            arguments: {
-                INPUT: {
-                    type: ArgumentType.STRING, 
-                    menu: 'INPUT',
-                    defaultValue: 'o1'
-                },     
-            }
-		}
+        	b.getBlock(),
+			b.getBlock_setLamp(),
+			b.getBlock_doSetMotorSpeed(),
+			b.getBlock_doSetMotorSpeedDir(),
+			{
+				opcode: 'hat',
+				blockType: BlockType.COMMAND,
+				text: "Motor",
+			},
+			{
+				opcode: 'Motor',
+				blockType: BlockType.HAT,
+				text: "Motor",
+			},
+			{
+				opcode: 'input',
+				text: 'set [INPUT]',
+				blockType: BlockType.EVENT,
+				arguments: {
+					INPUT: {
+						type: ArgumentType.STRING, 
+						menu: 'INPUT',
+						defaultValue: 'o1'
+					},     
+				}
+			}
         ],
 
         menus: {
-            
-                ONOFFSTATE: [
-					b.getMenu()
-				],
-				outputID: [
-					{text: 'O1', value: 'o1'} ,
-					{text: 'O2', value: 'o2'}
-					],
-                inputID: [
-	 				{text: 'O1', value: 'o1'}, {text: 'O2', value: 'o2'},
-		    		{text: 'O3', value: 'o3'}, {text: 'O4', value: 'o4'}
-				],
-				inputModes: [
-					{text: 'Digital voltage', value: 'd10v'}, {text: 'Digital resistance', value: 'd5k'},
-		    		{text: 'Analogue voltage', value: 'a10v'}, {text: 'Analogue resistance', value: 'a5k'}
-				],
-				inputAnalogSensorTypes: [
-					{text: 'Color Sensor', value: 'sens_color'}, {text: 'NTC Resistor', value: 'sens_ntc'},
-		    		{text: 'Photo Resistor', value: 'sens_photo'}
-				],
-				inputDigitalSensorTypes: [
-					{text: 'Button', value: 'sens_button'}, {text: 'Light barrier', value: 'sens_lightBarrier'},
-		    		{text: 'Reed contact', value: 'sens_reed'}, {text: 'Trail Sensor', value:'sens_trail'}
-				],
-				inputDigitalSensorChangeTypes: [
-					{text: 'open', value: 'open'}, {text: 'closed', value: 'closed'}
-				], 
-				motorDirection: [
-					{text: 'forward', value: 1}, {text: 'backwards', value: -1}
-				]
+            ONOFFSTATE: [
+				b.getMenu()
+			],
+			outputID: [
+				{text: 'O1', value: 'o1'},
+				{text: 'O2', value: 'o2'}
+			],
+            inputID: [
+	 			{text: 'O1', value: 'o1'}, {text: 'O2', value: 'o2'},
+		    	{text: 'O3', value: 'o3'}, {text: 'O4', value: 'o4'}
+			],
+			inputModes: [
+				{text: 'Digital voltage', value: 'd10v'}, {text: 'Digital resistance', value: 'd5k'},
+		    	{text: 'Analogue voltage', value: 'a10v'}, {text: 'Analogue resistance', value: 'a5k'}
+			],
+			inputAnalogSensorTypes: [
+				{text: 'Color Sensor', value: 'sens_color'}, {text: 'NTC Resistor', value: 'sens_ntc'},
+		    	{text: 'Photo Resistor', value: 'sens_photo'}
+			],
+			inputDigitalSensorTypes: [
+				{text: 'Button', value: 'sens_button'}, {text: 'Light barrier', value: 'sens_lightBarrier'},
+		    	{text: 'Reed contact', value: 'sens_reed'}, {text: 'Trail Sensor', value:'sens_trail'}
+			],
+			inputDigitalSensorChangeTypes: [
+				{text: 'open', value: 'open'}, {text: 'closed', value: 'closed'}
+			], 
+			motorDirection: [
+				{text: 'forward', value: 1}, {text: 'backwards', value: -1}
+			]
 	    }
         };
     }
 
 
-    
 	hat1(args) {
 		if(a==5){
 			alert(a);
